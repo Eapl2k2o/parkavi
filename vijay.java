daf490c150300b3e7146ffecb01b46b1053f873f
Eapl2k2o <150134220+Eapl2k2o@users.noreply.github.com>
2026-03-28 12:09:26 +0530

    Add a line to check audibility in vijay.java

--- vijay.java
+++ vijay.java
@@ -3,6 +3,7 @@ class vijay
     public static void main(String[]args)
     {
 System.out.println("hi sir how are you");
+      System.out.println("parkavi mam am i audible or not");
       
     }
   }
